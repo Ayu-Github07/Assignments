98c8240bd1dff2a51766495a0b31149b2c4d0874
Ayush Agrawal <07ayush0707@gmail.com>
2022-02-20 13:28:17 +0530

    Adding Spring Security Assignments

---
+++ 8. Spring Security/Assignments/assignment1/src/main/java/com/example/assignment1/config/MyConfig.java
@@ -0,0 +1,28 @@
+package com.example.assignment1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class MyConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("Ayush Agrawal")
+                .password(this.passwordEncoder().encode("Ayush@12345"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+}
